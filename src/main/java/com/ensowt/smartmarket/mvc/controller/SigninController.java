1624624d08af3597a920438ad180975342bce844
Akshat Kumar Vaish <akshatvaish.public@gmail.com>
2014-04-10 21:15:50 +0530

    License statement added to code

--- src/main/java/com/ensowt/smartmarket/mvc/controller/SigninController.java
+++ src/main/java/com/ensowt/smartmarket/mvc/controller/SigninController.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of SmartMarket.
+ *
+ * SmartMarket is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ *
+ * SmartMarket is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SmartMarket.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.ensowt.smartmarket.mvc.controller;
 
 import org.springframework.stereotype.Controller;
@@ -10,7 +26,7 @@ public class SigninController {
 
 	@RequestMapping("")
 	public ModelAndView showSignInPage(ModelAndView modelAndView) {
-		
+
 		modelAndView.setViewName("merchantSignin");
 
 		return modelAndView;
